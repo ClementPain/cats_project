5b69af41a6a573a0c0ff09bf17efba230a75677c
clement <clementpain@outlook.com>
2020-08-31 12:03:03 +0200

    Mise en place Pipeline

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root 'cat_pictures#index'
   devise_for :users
-  resources:cat_pictures
+  resources:cat_pictureshe
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
