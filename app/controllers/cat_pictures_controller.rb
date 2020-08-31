0190e6ad15cdbbea44c04c03fb429efe55526932
oumasma <oumasma93@hotmail.fr>
2020-08-31 19:52:29 +0400

    ajout des cartes produits à l'index

--- app/controllers/cat_pictures_controller.rb
+++ app/controllers/cat_pictures_controller.rb
@@ -1,4 +1,14 @@
 class CatPicturesController < ApplicationController
+
+
+    
     def index
+    	@cat_pictures = CatPicture.all
     end
+
+    def show
+		@cat_picture = CatPicture.find(params[:id])
+	end
+
+	
 end
