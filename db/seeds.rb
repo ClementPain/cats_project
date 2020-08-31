50cf0747e913f3e8cf7f8504bab1489b6b43896b
fatoumakara <fatouma_karamoko@hotmail.com>
2020-08-31 15:41:44 +0200

    Ajout du seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+    Cat_picture.create(title: "J'ai faiiiiim",  
+     description:Faker::GreekPhilosophers.quote, 
+     price:rand(10..60),
+     image_url: "https://cdn.radiofrance.fr/s3/cruiser-production/2018/12/9f19b228-269f-4995-ba7f-eda054945811/1136_gettyimages-155607257.jpg")
+
+    Cat_picture.create(title: "Chut, on dort! ",  
+     description:Faker::GreekPhilosophers.quote, 
+     price:rand(10..60),
+     image_url: "https://lh3.googleusercontent.com/proxy/XDSEwBcm4DtxvQBZMrLBohC2OLbPFqkrjX7LYa-9a7idvaPMrJXXmIglRW6IG7BuKsa5pemxeR-bhz09oqlDKq94_UVzUmhnI8rU-WI1_L-v5tzU1edlbBk7")
+
+
+    Cat_picture.create(title: "Saluuuut!",  
+     description:Faker::GreekPhilosophers.quote, 
+     price:rand(10..60),
+     image_url: "https://lemagduchat.ouest-france.fr/images/dossiers/2019-10/chaton-111206.jpg")
+
+
+    Cat_picture.create(title: "Ca baille",  
+     description:Faker::GreekPhilosophers.quote, 
+     price:rand(10..60),
+     image_url: "https://i.pinimg.com/originals/4b/f6/86/4bf68667464e94d66d54af01a6e1141b.jpg")
+
+
+    Cat_picture.create(title: "Tic et Toc",  
+     description:Faker::GreekPhilosophers.quote, 
+     price:rand(10..60),
+     image_url: "https://i.pinimg.com/564x/ec/b2/c6/ecb2c6e92ba1b6d7fe9e895fbc463381.jpg")
+
+
+    Cat_picture.create(title: "Chat and Chill",  
+     description:Faker::GreekPhilosophers.quote, 
+     price:rand(10..60),
+     image_url: "https://i.pinimg.com/originals/96/5f/d7/965fd7d6b859c409a5aed1063de2f833.jpg")
+
+    Cat_picture.create(title: "Bébé chat",  
+     description:Faker::GreekPhilosophers.quote, 
+     price:rand(10..60),
+     image_url: "https://www.pattesetmoustaches.com/wp-content/uploads/2016/06/chat-trop-mignion.jpg")
+
+    Cat_picture.create(title: "Les 6 mousquetaires",  
+     description:Faker::GreekPhilosophers.quote, 
+     price:rand(10..60),
+     image_url: "https://i.pinimg.com/originals/8b/02/d4/8b02d48a697200e2f6d60efcfd42f74c.jpg")
+
