7bb1ad144664c8f8155852954bfb8bce939b28ce
clement <clementpain@outlook.com>
2020-08-31 17:34:43 +0200

    index

--- app/controllers/cat_pictures_controller.rb
+++ app/controllers/cat_pictures_controller.rb
@@ -1,4 +1,8 @@
 class CatPicturesController < ApplicationController
     def index
     end
+
+    def show
+        @cat = CatPicture.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'cat_pictures#index'
   devise_for :users
-  resources:cat_pictureshe
+  resources:cat_pictures
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
