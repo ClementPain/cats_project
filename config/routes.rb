0bbe33d52ab070fa2850a5401955aed4049d0bf2
clement <clementpain@outlook.com>
2020-09-01 10:45:31 +0200

    migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'cat_pictures#index'
   devise_for :users
-  resources:cat_pictures do
+  resources:cat_pictures
+  
+  resource :user do
     resources:carts
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
