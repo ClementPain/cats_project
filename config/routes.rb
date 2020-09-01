cb7265d85a06c0fdafbb48eece43d51880c425ab
clement <clementpain@outlook.com>
2020-09-01 09:19:21 +0200

    branch carts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  
   root 'cat_pictures#index'
   devise_for :users
-  resources :cat_pictures
-  resources :carts
+  resources:cat_pictures do
+    resources:carts
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
