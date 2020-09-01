7cc6352bc952085f5ecc5305603348b259eb3719
oumasma <oumasma93@hotmail.fr>
2020-09-01 11:13:42 +0400

    ajout de la route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'carts/show'
-  get 'carts/create'
-  get 'carts/update'
-  get 'carts/destroy'
+  
   root 'cat_pictures#index'
   devise_for :users
-  resources:cat_pictures
+  resources :cat_pictures
+  resources :carts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
