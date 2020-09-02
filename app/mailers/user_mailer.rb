31863ec65bb0cd2598cd325517033af9e1cfa402
clement <clementpain@outlook.com>
2020-09-02 20:49:45 +0200

    Test mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,7 @@ class UserMailer < ApplicationMailer
     def welcome_email(user)
         @user = user      #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
         @url  = 'https://catsproject33.herokuapp.com/'       #on définit une variable @url qu'on utilisera dans la view d’e-mail     
-        mail(to: @user.email, subject: 'Bienvenue chez nous !')       # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.    
+        mail(to: @user.email, subject: 'Bienvenue chez nous !',delivery_method_options: delivery_options)       # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.  
+        delivery_options = { api_key: ENV["MAILJET_API_KEY"], secret_key: ENV['MAILJET_SECRET_KEY'] }
     end
 end
