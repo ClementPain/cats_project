c2f8faa6fc631ee1a16b9ab2c6366cea04d05e59
clement <clementpain@outlook.com>
2020-09-02 18:55:50 +0200

    ajout de actionmailer et mailjet

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+    default from: 'kittyswall@gmail.com'
+
+    def welcome_email(user)
+        @user = user      #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+        @url  = 'https://catsproject33.herokuapp.com/'       #on définit une variable @url qu'on utilisera dans la view d’e-mail     
+        mail(to: @user.email, subject: 'Bienvenue chez nous !')       # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.    
+    end
+end
