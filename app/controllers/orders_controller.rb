adc82e3ee58f60d4ed9e96e74e04f3971adb8f48
clement <clementpain@outlook.com>
2020-09-03 18:20:15 +0200

    mails

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,3 @@
 class OrdersController < ApplicationController
-    def create
-        # créer une commande liée au client
-        @order=Order.new(user:current_user)
-        @order_details = current_user.cart.cat_pictures
-        
 
-        # créer x éléments de la commande en récupérant les éléments du panier
-        @order_details.each do |image|
-            OrdersDetail.create(order:@order, cat_picture:image)
-        end
-        
-        @order.create
-
-        #détruire le panier
-        current_user.cart.line_cat_pictures.destroy
-        current_user.cart.destroy
-        
-        #renvoyer l'utilisateur vers ...
-        redirect_to root_path, alert: "Votre panier est bien validé"
-    end
 end
--- app/controllers/orders_details_controller.rb
+++ app/controllers/orders_details_controller.rb
@@ -1,10 +1,7 @@
 class OrdersDetailsController < ApplicationController
-end
-class OrdersDetailsController < ApplicationController
 
   Dotenv.load
 
   def create
     Stripe.new.perform
-end
 end
